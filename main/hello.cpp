4bec38708c2e02bc0963484487d954d51aaa9567
Punarv Dinakar <tranquilblue91@gmail.com>
2023-02-21 00:57:22 +0530

    Second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "Hello, Universe\n";
+  cout << "Hello, Jonkins\n";
+  cout << "I am awesome\n";
   return 0;
 }
